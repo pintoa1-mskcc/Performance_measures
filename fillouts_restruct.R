6e7156cbcc0930366642b1486abadf8b03894e8e
pintoa1-mskcc <pintoa1@mskcc.org>
2021-12-16 12:19:24 -0500

    adding getbase counts, IS ALTERNATIVE ALLELE NEVER RETURNED OR WAS IT MY TEST RUN

--- fillouts_restruct.R
+++ fillouts_restruct.R
@@ -33,35 +33,11 @@ if(opt$performance_measures) {
 }
 
 
-
-restructure_mafs <- function(x) {
-  temp <- fread(x,data.table=FALSE)
-  colnam <- colnames(temp)
-  temp <- temp  %>% mutate(variant_loc_to_merge = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Seq_Allele1))
-  
-  ### We will calcaulate clonality for test var tags in case they are false positives, but true positives must match between ground and test var tags
-  n <- temp[grepl("_N00|GCT02|uttcc",temp$Tumor_Sample_Barcode),] #2 special old samples with no N in the normal
-  t <- temp[!grepl("_N00|GCT02|uttcc",temp$Tumor_Sample_Barcode),]
-  colnames(n) <- gsub("^t_","n_",colnames(n))
-  
-  n$Matched_Norm_Sample_Barcode <- n$Tumor_Sample_Barcode 
-  n$Tumor_Sample_Barcode <- NULL
-  t$Matched_Norm_Sample_Barcode <- NULL
-  n <- n %>% select(c( colnames(n)[grepl('^n',colnames(n))],'Matched_Norm_Sample_Barcode','variant_loc_to_merge'))
-  t <- t[,!grepl('^n',colnames(t))]
-  
-  
-  temp2 <- merge(t,n,by='variant_loc_to_merge')
-  
-  temp2 <- temp2  %>% mutate(var_tag = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Seq_Allele1,':',Tumor_Sample_Barcode), 
+performance_measures_expected_formatting <- function(file,opt){
+  fillout_maf <- fread(file,data.table=FALSE)
+  fillout_maf <- fillout_maf  %>% mutate(var_tag = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Seq_Allele1,':',Tumor_Sample_Barcode), 
                              TAG = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Sample_Barcode))
-  temp2$variant_loc_to_merge <- NULL
   
-  return(temp2)
-}
-
-
-performance_measures_expected_formatting <- function(fillout_maf,opt){
   fillout_maf$Tumor_Seq_Allele2 <- fillout_maf$Tumor_Seq_Allele1
   fillout_maf$genotyped_variant_freq <- fillout_maf$t_variant_frequency
   fillout_maf$t_variant_frequency <- NULL
@@ -74,7 +50,7 @@ performance_measures_expected_formatting <- function(fillout_maf,opt){
   n <- colnames(fillout_maf)[grepl('^n',colnames(fillout_maf))]
   maf_with_more_info <- maf_with_more_info[, colnames(maf_with_more_info) %nin% c(t,n)]
   fillout_maf <- fillout_maf[,c('var_tag',t,n,'genotyped_variant_freq')]
-  return(merge(fillout_maf,maf_with_more_info, by = 'var_tag',all.y = FALSE))
+  return(merge(fillout_maf,maf_with_more_info, by = 'TAG',all.y = FALSE))
   
 }
 
