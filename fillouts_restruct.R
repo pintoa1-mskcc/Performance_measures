27d1cf8f08a5b2d9a7bbd9786e43bc25ddbe8950
Alexandria Pinto <pintoa1@mskcc.org>
2023-03-13 11:46:09 -0400

    additional var add

--- fillouts_restruct.R
+++ fillouts_restruct.R
@@ -1,4 +1,4 @@
-suppressPackageStartupMessages({library(argparse) 
+suppressPackageStartupMessages({library(argparse)
 library(data.table)
 library(dplyr)
 library(stringr)})
@@ -18,6 +18,7 @@ parser$add_argument("-j", "--juno" , type = 'logical',default = FALSE)
 parser$add_argument("-b","--bed_file", type = "character", default = NULL, help = "If you wish to use a bed file for targetted performance measures.")
 parser$add_argument("-s", "--script",type = 'character',default = getwd(),help = 'If running performance measures, expects Rscript to be in working directory If not, please specify directory.')
 parser$add_argument('-c','--called_directory',type = "character", default=NULL, help = 'Specify the directory containing the performance measures for CALLED MAF results')
+parser$add_argument('-v','--additional_variables',type = "character", default=NULL, help = '')
 opt=parser$parse_args()
 
 
@@ -29,15 +30,15 @@ if(is.null(opt$directory)){
 if(opt$performance_measures) {
   if(is.null(opt$maf_dir) | is.null(opt$test_directory) ){
     stop('Something went wrong! A unified MAF directory AND a test directory is required if you wish to run performance measure.')
-  } 
+  }
 }
 
 
 performance_measures_expected_formatting <- function(file,opt){
   fillout_maf <- fread(file,data.table=FALSE)
-  fillout_maf <- fillout_maf  %>% mutate(var_tag = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Seq_Allele1,':',Tumor_Sample_Barcode), 
+  fillout_maf <- fillout_maf  %>% mutate(var_tag = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Seq_Allele1,':',Tumor_Sample_Barcode),
                              TAG = str_c(Chromosome,':',Start_Position,':',End_Position,':',Reference_Allele,':',Tumor_Sample_Barcode))
-  
+
   fillout_maf$Tumor_Seq_Allele2 <- fillout_maf$Tumor_Seq_Allele1
   fillout_maf$genotyped_variant_freq <- fillout_maf$t_variant_frequency
   fillout_maf$t_variant_frequency <- NULL
@@ -51,7 +52,7 @@ performance_measures_expected_formatting <- function(file,opt){
   maf_with_more_info <- maf_with_more_info[, colnames(maf_with_more_info) %nin% c(t,n)]
   fillout_maf <- fillout_maf[,c('var_tag',t,n,'genotyped_variant_freq')]
   return(merge(maf_with_more_info,fillout_maf, by = 'var_tag',all.y = FALSE))
-  
+
 }
 
 format_output_name <- function(opt,suffix) {
@@ -70,45 +71,53 @@ if(opt$performance_measures) {
   opt$maf_dir <-  paste0(opt$maf_dir,'/')
   test_files <- list.files(opt$test_directory)
   test_files <- paste0(opt$test_directory,test_files)
-  
+
   fillex_ground <- do.call(rbind,lapply(ground_files,function(file) {
     write(file,stderr())
-    output <- performance_measures_expected_formatting(file,opt) 
-    
+    output <- performance_measures_expected_formatting(file,opt)
+
     return(output)
   } ))
-  
+
   fillex_test <- do.call(rbind,lapply(test_files,function(file) {
-    output <- performance_measures_expected_formatting(file,opt) 
-   
+    output <- performance_measures_expected_formatting(file,opt)
+
     return(output)
   } ))
-  
-  
-  
-  
+
+
+
+
   test_file_name <- format_output_name(opt,'genotyped_test.maf')
 
   ground_file_name <- format_output_name(opt,'genotyped_ground.maf')
-  
-  
+
+
   write.table(fillex_ground,ground_file_name,quote = FALSE, row.names = FALSE,sep = "\t")
-  
+
   write.table(fillex_test,test_file_name,quote = FALSE, row.names = FALSE,sep = "\t")
   name_test <- basename(opt$test_directory)
   name_ground <- basename(opt$ground_directory)
   bsub_command <- paste0('bsub -o ', opt$directory,'logs/',opt$out_prefix, '_performance_measure_fillout.out -n 2 -R "rusage[mem=8]" -W 1:59 "Rscript ',opt$script,'performance_measure_script.R -g ', ground_file_name,' -t ', test_file_name, ' -d ',opt$directory,' -s ',name_test,' -n ',name_ground, ' -c ', opt$directory,' -m  -p  -o fillout_', opt$out_prefix)
-  
+
   if(!is.null(opt$called_directory)){
     bsub_command <- paste0(bsub_command, ' -c ', opt$called_directory, ' -u ', opt$out_prefix)
   }
-  
-  
+
+
   if(is.null(opt$bed_file)){
     bsub_command <- paste0(bsub_command,'"')
   } else {
     bsub_command <- paste0(bsub_command, ' -b ',opt$bed_file ,'"')
   }
-  
+
+
+  if(is.null(opt$additional_variables)){
+    bsub_command <- paste0(bsub_command,'"')
+  } else {
+    bsub_command <- paste0(bsub_command, ' -v ',opt$additional_variables ,'"')
+  }
+
+
   system(bsub_command)
-} 
+}
